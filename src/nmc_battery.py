74c3bfaff2972856828845aef0c717af22b759b0
ll2967@mci4me.at <ll2967@mci4me.at>
2026-07-15 13:14:22 +0200

    fix(battery): fixed handling of below zero currents on battery models

--- src/nmc_battery.py
+++ src/nmc_battery.py
@@ -16,3 +16,5 @@ class NMCBatteryPack(BatteryPack):
         if current > 0:
             open_circuit_voltage = self.Vmin + (self.soc**0.834) * (self.Vmax - self.Vmin)
             return open_circuit_voltage - self.R_int * current
+        else:
+            return self.Vmin + (self.soc**0.3) * (self.Vmax - self.Vmin)
